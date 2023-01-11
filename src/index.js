bc0ba5b36309778edce64f9a067468814fe988f5
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-12 01:29:15 +0200

    as

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./Routes/Home";
 import About from "./Routes/About";
@@ -11,29 +8,27 @@ import Contact from "./Routes/Contact";
 import Work from "./Routes/Work";
 import Project from "./Routes/ProjectRoute";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        { path: "about", element: <About /> },
-        {
-          path: "work",
-          element: <Work />,
-        },
-        { path: "contact", element: <Contact /> },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      { path: "about", element: <About /> },
+      {
+        path: "work",
+        element: <Work />,
+      },
+      { path: "contact", element: <Contact /> },
 
-        {
-          path: "work/:project",
-          element: <Project />,
-        },
-      ],
-    },
-  ]
-);
+      {
+        path: "work/:project",
+        element: <Project />,
+      },
+    ],
+  },
+]);
 root.render(<RouterProvider router={router} />);
