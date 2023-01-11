2dc8fc8a6209b4c6057544807550679c110e939e
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-11 02:59:46 +0200

    ssd

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -214,7 +214,7 @@ function Home() {
           spacing="20px"
           alignItems={"center"}
           display={{ xs: "flex", md: "none" }}
-          sx={{ position: "absolute", bottom: "10%" }}
+          sx={{ position: "absolute", bottom: "12%" }}
         >
           <Box
             component={"p"}
