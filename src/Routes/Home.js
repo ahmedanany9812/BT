72d005153b3b3c6ad6758ba34f14ccb059e733c0
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-11 14:15:13 +0200

    recode

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -4,164 +4,121 @@ import RoutesLink from "../Data/RoutesLink.json";
 function Home() {
   const { fonts } = CustomTheme;
   return (
-    <Box
-      position={"relative"}
-      padding="1rem"
-      height={"100vh"}
-      overflow={{ xs: "hidden", md: "auto" }}
-    >
-      <Container maxWidth="xl" sx={{ height: "100%" }}>
-        <Box
-          letterSpacing="5px"
-          component={"p"}
-          fontWeight={300}
-          fontFamily={fonts.body}
-          fontSize="15px"
-          padding={{ xs: "0", md: "2rem" }}
-        >
-          BT.
+    <Container maxWidth="xl">
+      <Box
+        height={"100vh"}
+        padding="3rem"
+        overflow={{ xs: "scroll", md: "hidden" }}
+      >
+        <Box component={"header"} height="20%">
+          <Box
+            component={"p"}
+            letterSpacing="5px"
+            fontSize={"15px"}
+            fontFamily={fonts.body}
+            fontWeight={300}
+          >
+            BT.
+          </Box>
         </Box>
-        <Box
-          height={"85vh"}
-          sx={{
-            display: { xs: "none", md: "flex" },
-            alignItems: "flex-end",
-          }}
+        <Stack
+          height={"80%"}
+          direction={{ xs: "column", md: "row" }}
+          alignItems={{ xs: "flex-start", md: "flex-end" }}
           paddingBottom={{ xs: "0", md: "2rem" }}
-          paddingTop={"0"}
         >
           <Stack
-            direction={{ xs: "column", md: "row" }}
-            alignItems={{ xs: "flex-start", md: "flex-end" }}
-            justifyContent={"center"}
+            direction={"column"}
+            spacing="20px"
+            maxWidth={{ xs: "100%", md: "25%" }}
+            marginBottom={"20px"}
           >
+            <Box
+              component={"p"}
+              fontWeight={500}
+              fontFamily={fonts.hom}
+              fontSize="15px"
+              letterSpacing="2px"
+            >
+              AHMED ANANY
+            </Box>
+            <Box
+              component={"p"}
+              fontWeight={300}
+              fontFamily={fonts.hom}
+              fontSize="15px"
+              lineHeight={"1.9em"}
+            >
+              A junior fullstack JS developer since 2022 and Data Science
+              Student
+            </Box>
+            <Box
+              component={"p"}
+              fontWeight={300}
+              fontFamily={fonts.hom}
+              fontSize="15px"
+              lineHeight={"1.9em"}
+            >
+              Working As freelancer and Looking for a new apportunity
+            </Box>
             <Stack
-              direction={"column"}
+              direction={"row"}
               spacing="20px"
-              maxWidth={{ xs: "100%", md: "25%" }}
-              marginBottom={"20px"}
+              alignItems={"center"}
+              display={{ xs: "none", md: "flex" }}
             >
               <Box
                 component={"p"}
-                fontWeight={500}
-                fontFamily={fonts.hom}
-                fontSize="15px"
-                letterSpacing="2px"
+                fontWeight={200}
+                fontFamily={fonts.body}
+                fontSize="14px"
+                letterSpacing={"2px"}
               >
-                AHMED ANANY
+                TW
               </Box>
               <Box
                 component={"p"}
-                fontWeight={300}
-                fontFamily={fonts.hom}
-                fontSize="15px"
-                lineHeight={"1.9em"}
+                fontWeight={200}
+                fontFamily={fonts.body}
+                fontSize="14px"
+                letterSpacing={"2px"}
               >
-                A junior fullstack JS developer since 2022 and Data Science
-                Student
+                FB
               </Box>
               <Box
                 component={"p"}
-                fontWeight={300}
-                fontFamily={fonts.hom}
-                fontSize="15px"
-                lineHeight={"1.9em"}
+                fontWeight={200}
+                fontFamily={fonts.body}
+                fontSize="14px"
+                letterSpacing={"2px"}
               >
-                Working As freelancer and Looking for a new apportunity
+                LI
               </Box>
-              <Stack
-                direction={"row"}
-                spacing="20px"
-                alignItems={"center"}
-                display={{ xs: "none", md: "flex" }}
-              >
-                <Box
-                  component={"p"}
-                  fontWeight={200}
-                  fontFamily={fonts.body}
-                  fontSize="14px"
-                  letterSpacing={"2px"}
-                >
-                  TW
-                </Box>
-                <Box
-                  component={"p"}
-                  fontWeight={200}
-                  fontFamily={fonts.body}
-                  fontSize="14px"
-                  letterSpacing={"2px"}
-                >
-                  FB
-                </Box>
-                <Box
-                  component={"p"}
-                  fontWeight={200}
-                  fontFamily={fonts.body}
-                  fontSize="14px"
-                  letterSpacing={"2px"}
-                >
-                  LI
-                </Box>
-              </Stack>
-            </Stack>
-            <Stack direction={"column"} marginLeft={{ xs: "0px", md: "6%" }}>
-              {RoutesLink.map((ro, i) => {
-                return (
-                  <Box
-                    component={"p"}
-                    fontWeight={300}
-                    fontFamily={fonts.font5}
-                    fontSize={{ xs: "15vw", md: "12vw" }}
-                    key={i}
-                    lineHeight="0.95em"
-                    letterSpacing={{ xs: "-1px", md: "-5px" }}
-                  >
-                    {ro.tag}
-                  </Box>
-                );
-              })}
             </Stack>
           </Stack>
-        </Box>
-        <Stack
-          direction={"column"}
-          spacing="20px"
-          maxWidth={{ xs: "100%", md: "25%" }}
-          marginBottom={"20px"}
-          marginTop="5rem"
-        >
-          <Box
-            component={"p"}
-            fontWeight={500}
-            fontFamily={fonts.hom}
-            fontSize="15px"
-            letterSpacing="2px"
-          >
-            AHMED ANANY
-          </Box>
-          <Box
-            component={"p"}
-            fontWeight={300}
-            fontFamily={fonts.hom}
-            fontSize="15px"
-            lineHeight={"1.9em"}
-          >
-            A junior fullstack JS developer since 2022 and Data Science Student
-          </Box>
-          <Box
-            component={"p"}
-            fontWeight={300}
-            fontFamily={fonts.hom}
-            fontSize="15px"
-            lineHeight={"1.9em"}
-          >
-            Working As freelancer and Looking for a new apportunity
-          </Box>
+          <Stack direction={"column"} marginLeft={{ xs: "0px", md: "6%" }}>
+            {RoutesLink.map((ro, i) => {
+              return (
+                <Box
+                  component={"p"}
+                  fontWeight={300}
+                  fontFamily={fonts.font5}
+                  fontSize={{ xs: "15vw", md: "12vw" }}
+                  key={i}
+                  lineHeight="0.95em"
+                  letterSpacing={{ xs: "-1px", md: "-5px" }}
+                >
+                  {ro.tag}
+                </Box>
+              );
+            })}
+          </Stack>
           <Stack
             direction={"row"}
             spacing="20px"
             alignItems={"center"}
-            display={{ xs: "none", md: "flex" }}
+            display={{ xs: "flex", md: "none" }}
+            marginTop="5rem"
           >
             <Box
               component={"p"}
@@ -192,60 +149,8 @@ function Home() {
             </Box>
           </Stack>
         </Stack>
-        <Stack direction={"column"} marginLeft={{ xs: "0px", md: "6%" }}>
-          {RoutesLink.map((ro, i) => {
-            return (
-              <Box
-                component={"p"}
-                fontWeight={300}
-                fontFamily={fonts.font5}
-                fontSize={{ xs: "15vw", md: "12vw" }}
-                key={i}
-                lineHeight="0.95em"
-                letterSpacing={{ xs: "-1px", md: "-5px" }}
-              >
-                {ro.tag}
-              </Box>
-            );
-          })}
-        </Stack>
-        <Stack
-          direction={"row"}
-          spacing="20px"
-          alignItems={"center"}
-          display={{ xs: "flex", md: "none" }}
-          sx={{ position: "absolute", bottom: "11%" }}
-        >
-          <Box
-            component={"p"}
-            fontWeight={200}
-            fontFamily={fonts.body}
-            fontSize="14px"
-            letterSpacing={"2px"}
-          >
-            TW
-          </Box>
-          <Box
-            component={"p"}
-            fontWeight={200}
-            fontFamily={fonts.body}
-            fontSize="14px"
-            letterSpacing={"2px"}
-          >
-            FB
-          </Box>
-          <Box
-            component={"p"}
-            fontWeight={200}
-            fontFamily={fonts.body}
-            fontSize="14px"
-            letterSpacing={"2px"}
-          >
-            LI
-          </Box>
-        </Stack>
-      </Container>
-    </Box>
+      </Box>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Contact from "./Routes/Contact";
+import Work from "./Routes/Work";
 import Project from "./Routes/ProjectRoute";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route element={<Home />} index />
-      <Route path="/:project" element={<Project />} />
+      <Route element={<About />} path="about" />
+      <Route element={<Work />} path="work">
+        <Route path=":project" element={<Project />} />
+      </Route>
+      <Route element={<Contact />} path="contact" />
     </Route>
   )
 );
