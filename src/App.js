942bd4a1a664d90ff62a728585dd375b5dfeccb1
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-23 23:27:53 +0200

    dda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import { Box, Container } from "@mui/material";
-import { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import "./Style/app.scss";
 import TopScroll from "./Utilities/TopScroll";
-import UseWindowSize from "./Utilities/WindowSize";
 function App() {
-  const Size = UseWindowSize();
-  const AppContainer = useRef();
-  // useEffect(() => {
-  //   if (Size.width > 800) {
-  //     AppContainer.current.style.position = "fixed";
-  //   } else {
-  //     AppContainer.current.style.position = "relative";
-  //   }
-  // });
   return (
-    <Box position={"relative"} id="App" ref={AppContainer}>
-      {/* <TopScroll />
-      <Container maxWidth="xl"> */}
+    <Box position={"relative"} id="App">
+      <TopScroll />
+      <Container maxWidth="xl">
         <Outlet />
-      {/* </Container> */}
+      </Container>
     </Box>
   );
 }
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,41 +1,14 @@
 import { Box } from "@mui/material";
 import WorkSection from "../Components/WorkSection";
-import { useEffect, useRef } from "react";
-import UseWindowSize from "../Utilities/WindowSize";
 import HeadSection from "../Components/HeadSection";
+import SmoothScroll from "../Utilities/SmoothScroll";
 function Home() {
-  const ScrollContainer = useRef();
-  const Size = UseWindowSize();
-  useEffect(() => {
-    document.body.style.height = `${
-      ScrollContainer.current.getBoundingClientRect().height
-    }px`;
-  }, [Size.height]);
-  const SkewConfigs = {
-    ease: 0.1,
-    current: 0,
-    previous: 0,
-    rounded: 0,
-  };
-  const SkewScrolling = () => {
-    SkewConfigs.current = window.scrollY;
-    SkewConfigs.previous +=
-      (SkewConfigs.current - SkewConfigs.previous) * SkewConfigs.ease;
-    SkewConfigs.rounded = Math.round(SkewConfigs.previous * 100) / 100;
-    const Diff = SkewConfigs.current - SkewConfigs.rounded;
-    const acce = Diff / Size.width;
-    const Velocity = +acce;
-    const skew = Velocity * 7.5;
-    ScrollContainer.current.style.transform = `translateY(-${SkewConfigs.rounded}px) skewY(${skew}deg)`;
-    requestAnimationFrame(() => SkewScrolling());
-  };
-  useEffect(() => {
-    requestAnimationFrame(() => SkewScrolling());
-  });
   return (
-    <Box ref={ScrollContainer} className="Scroll">
-      <HeadSection />
-      <WorkSection />
+    <Box>
+      <SmoothScroll>
+        <HeadSection />
+        <WorkSection />
+      </SmoothScroll>
     </Box>
   );
 }
