1331eacf374f37f8bc7b6d852fa2a19e02b9b30b
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-23 23:43:22 +0200

    adw

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -26,11 +26,7 @@ function Home() {
     const acce = Diff / Size.width;
     const Velocity = +acce;
     const skew = Velocity * 7.5;
-
-    if (Size.width > 800) {
-      ScrollContainer.current.style.transform = `translateY(-${SkewConfigs.previous}px) skewY(${skew}deg)`;
-    } else {
-    }
+    ScrollContainer.current.style.transform = `translateY(-${SkewConfigs.previous}px) skewY(${skew}deg)`;
     requestAnimationFrame(() => SkewScrolling());
   };
   useEffect(() => {
