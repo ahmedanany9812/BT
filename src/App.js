cea317f52b01a969a6e980d494c3059efba61027
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2023-01-24 16:02:14 +0200

    aas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./Style/app.scss";
 import TopScroll from "./Utilities/TopScroll";
 function App() {
   return (
-    <Box position={"relative"} id="App" paddingX={{ xs: "0.5rem", sm: "2rem" }}>
+    <Box position={"relative"} id="App" paddingX={{ xs: "0.1rem", sm: "2rem" }}>
       <TopScroll />
       <Container maxWidth="xl">
         <Outlet />
